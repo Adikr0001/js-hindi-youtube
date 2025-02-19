329e52cda69d7cac11ba58f9d1eaefbb7eec4309
Adikr0001 <135831186+Adikr0001@users.noreply.github.com>
2025-02-19 19:05:01 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") // we are using nodejs, not browser
+
+// MDN 
+// ECMASCRIPT documentation 
+
+let name = "Aditya"
+let age = 19
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+//bigint for large numbers 
+// string => ""
+// boolean => true/false
+// null => standalone value (its an object)
+// undefined => value has not been assigned (type = undefined)
+// symbol => to find the uniqueness 
+
+// object 
+
+console.log(typeof age);
+
